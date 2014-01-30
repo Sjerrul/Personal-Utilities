3646b45ba62779cfdbdf2c016811982dcb9b11b9
Sjerrul <cerylwiltink@hotmail.com>
2014-01-30 13:08:10 +0100

    Added Chunking Extension for strings

--- Sjerrul.Utilities.Tests/Extentions/StringExtentionsTests.cs
+++ Sjerrul.Utilities.Tests/Extentions/StringExtentionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sjerrul.Utilities.Extentions;
+using System.Collections.Generic;
 
 namespace Sjerrul.Utilities.Tests.Extentions
 {
@@ -49,6 +50,144 @@ namespace Sjerrul.Utilities.Tests.Extentions
             string actual = s.ToInitials();
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_1_ReturnsCorrectList()
+        {
+            string input = "1";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "1"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_11_ReturnsCorrectList()
+        {
+            string input = "11";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "11"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_111_ReturnsCorrectList()
+        {
+            string input = "111";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "111"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_12_ReturnsCorrectList()
+        {
+            string input = "12";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "1",
+                "2"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_112_ReturnsCorrectList()
+        {
+            string input = "112";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "2"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_1122_ReturnsCorrectList()
+        {
+            string input = "1122";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_112233_ReturnsCorrectList()
+        {
+            string input = "112233";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22",
+                "33"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void ChunkBySimilarity_11223311_ReturnsCorrectList()
+        {
+            string input = "11223311";
+
+            IList<string> result = input.ChunkBySimilarity();
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22",
+                "33",
+                "11"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        private bool AreArraysEqual(IList<string> array1, IList<string> array2)
+        {
+            if (array1.Count != array2.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Count; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
---
+++ Sjerrul.Utilities/Extentions/StringExtentions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sjerrul.Utilities.Extentions
+{
+    public static class StringExtentions
+    {
+        public static int? ToNullableInt(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+            {
+                return null;
+            }
+
+            int returnValue;
+            if (Int32.TryParse(s, out returnValue))
+            {
+                return returnValue;
+            }
+
+            return (int?)null;
+        }
+
+        public static int ToInt(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+            {
+                throw new ArgumentException("Cannot cast to integer, string is empty or NULL");
+            }
+
+            int returnValue;
+            if (Int32.TryParse(s, out returnValue))
+            {
+                return returnValue;
+            }
+
+            throw new InvalidCastException(String.Format("Unable to cast string '{0}' to integer", s));
+        }
+
+        public static string ToInitials(this string s)
+        {
+            string normalized = s.ToUpper().Replace(".", "");
+
+            char[] characters = normalized.ToCharArray();
+            string joined = String.Join(".", characters);
+            return joined;
+        }
+
+        public static string MaxLength(this string s, int maxLength, string endWith = "...")
+        {
+            string shortend = s.Substring(0, maxLength);
+
+            return String.Format("{0}{1}", shortend, endWith);
+        }
+
+        public static IList<string> ChunkBySimilarity(this string s)
+        {
+            IList<string> retval = new List<string>();
+
+            int index = IndexOfFirstCharacterChange(s);
+            if (index < s.Length)
+            {
+                IList<string> t = s.Substring(index).ChunkBySimilarity();
+                retval = retval.Concat(t).ToList();
+            }
+
+            retval.Insert(0, s.Substring(0, index));
+
+            return retval;
+        }
+
+        private static int IndexOfFirstCharacterChange(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            if (input.Length == 1)
+            {
+                return 1;
+            }
+
+            int index = 0;
+            char current = input[index];
+            index++;
+            char next = input[index];
+
+            while (current == next)
+            {
+                index++;
+                if (index >= input.Length)
+                {
+                    break;
+                }
+
+                current = next;
+                next = input[index];
+            }
+
+            return index;
+        }
+    }
+}

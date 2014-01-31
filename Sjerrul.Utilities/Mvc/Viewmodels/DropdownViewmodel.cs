17f2ad26ed0eaa1ffdc158f0a0425f14a8ad8317
Sjerrul <cerylwiltink@hotmail.com>
2014-01-31 13:09:26 +0100

    Added DropdownViewModel and associated EditorTemplate in Mvc Namespace

---
+++ Sjerrul.Utilities/Mvc/Viewmodels/DropdownViewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sjerrul.Utilities.Mvc.Viewmodels
+{
+    class DropdownViewmodel
+    {
+        public string SelectedId { get; set; }
+        public IEnumerable<SelectListItem> Items { get; set; }
+    }
+}

4a9a409b003958b59760776bc69cc0a47f0c72ba
Sjerrul <cerylwiltink@hotmail.com>
2014-02-03 14:15:10 +0100

    Added ToAbsoluteUrl() string extension and RouteInfo Class to convert URL to RouteData

--- Sjerrul.Utilities/Extentions/StringExtentions.cs
+++ Sjerrul.Utilities/Extentions/StringExtentions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Sjerrul.Utilities.Extentions
 {
@@ -102,6 +103,35 @@ namespace Sjerrul.Utilities.Extentions
             }
 
             return index;
+        }
+
+        public static string ToAbsoluteUrl(this string relativeUrl)
+        {
+            if (string.IsNullOrEmpty(relativeUrl))
+            {
+                return relativeUrl;
+            }
+
+            if (HttpContext.Current == null)
+            {
+                return relativeUrl;
+            }
+
+            if (relativeUrl.StartsWith("/"))
+            {
+                relativeUrl = relativeUrl.Insert(0, "~");
+            }
+
+            if (!relativeUrl.StartsWith("~/"))
+            {
+                relativeUrl = relativeUrl.Insert(0, "~/");
+            }
+
+            var url = HttpContext.Current.Request.Url;
+            var port = url.Port != 80 ? String.Format(":{0}", url.Port) : String.Empty;
+
+            return String.Format("{0}://{1}{2}{3}",
+                url.Scheme, url.Host, port, VirtualPathUtility.ToAbsolute(relativeUrl));
         }
     }
 }

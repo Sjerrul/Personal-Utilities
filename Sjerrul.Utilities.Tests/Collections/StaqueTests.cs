66b0c77128e9a6f162dd604468aee2058356c30e
Sjerrul <cerylwiltink@hotmail.com>
2014-06-06 09:50:01 +0200

    Added TDD created Stack implementation, just for fun

---
+++ Sjerrul.Utilities.Tests/Collections/StaqueTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sjerrul.Utilities.Collections;
+using System.Diagnostics;
+
+namespace Sjerrul.Utilities.Tests.Collections
+{
+    [TestClass]
+    public class StaqueTests
+    {
+        [TestMethod]
+        public void Create_NoElementsAdded_CountIsZero()
+        {
+            Staque sut = new Staque();
+
+            int expected = 0;
+            int result = sut.Count;
+
+            Assert.AreEqual(expected, result);
+
+
+        }
+
+        [TestMethod]
+        public void Count_OneElementAdded_CountIsOne()
+        {
+            string test = "Test";
+            Staque sut = new Staque();
+            sut.Push(test);
+
+            int expected = 1;
+            int result = sut.Count;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Count_Elementremoved_CountIsZero()
+        {
+            string test = "Test";
+            Staque sut = new Staque();
+            sut.Push(test);
+            sut.Pop();
+
+            int expected = 0;
+            int result = sut.Count;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Count_Pop_ShouldReturnLastAddedElement()
+        {
+            string expected = "Test";
+            Staque sut = new Staque();
+
+            sut.Push(expected);
+            Assert.AreEqual(1, sut.Count);
+
+            object result = sut.Pop();
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(0, sut.Count);
+        }
+
+        [TestMethod]
+        public void Count_AddAndPopMultipleElements_ShouldReturnCorrectElements()
+        {
+            string string1 = "Hello";
+            string string2 = "World";
+            Staque sut = new Staque();
+
+            sut.Push(string1);
+            sut.Push(string2);
+            Assert.AreEqual(2, sut.Count);
+
+            object result = sut.Pop();
+            Assert.AreEqual(string2, result);
+            Assert.AreEqual(1, sut.Count);
+
+            result = sut.Pop();
+            Assert.AreEqual(string1, result);
+            Assert.AreEqual(0, sut.Count);
+        }
+
+        [TestMethod]
+        public void Count_Peek_ShouldReturnLastAddedElementWithoutRemoving()
+        {
+            string expected = "Test";
+            Staque sut = new Staque();
+
+            sut.Push(expected);
+            Assert.AreEqual(1, sut.Count);
+
+            object result = sut.Peek();
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(1, sut.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Pop_EmptyStack_ShouldThrowExceptions()
+        {
+            Staque sut = new Staque();
+
+            object result = sut.Pop();
+
+            //Assert ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Peek_EmptyStack_ShouldThrowExceptions()
+        {
+            Staque sut = new Staque();
+
+            object result = sut.Peek();
+
+            //Assert ExpectedException
+        }
+
+        [TestMethod]
+        public void Push_DifferentObjects_ShouldWorkNormally()
+        {
+            Staque sut = new Staque();
+
+            string s = "Test";
+            int i = 5;
+
+            sut.Push(s);
+            sut.Push(i);
+
+            Assert.AreEqual(i, sut.Pop());
+            Assert.AreEqual(s, sut.Pop());
+        }
+
+        [TestMethod]
+        public void StressTest()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            Staque sut = new Staque();
+            int numberOfElements = 5000000;
+            int i = 0;
+
+            stopWatch.Start();
+            for (i = 0; i < numberOfElements; i++)
+            {
+                sut.Push(i);
+            }
+            stopWatch.Stop();
+            long milliseconds = stopWatch.ElapsedMilliseconds;
+            Debug.WriteLine("Pushing {0} on the Staque: {1}ms", numberOfElements, milliseconds);
+
+            i--; //Last loop incremented i, but that never got pushed on the stack.
+            stopWatch.Reset();
+            stopWatch.Start();
+            while (sut.Count != 0)
+            {
+                int result = (int)sut.Pop();
+                Assert.AreEqual(i, result);
+                i--;
+            }
+            stopWatch.Stop();
+            milliseconds = stopWatch.ElapsedMilliseconds;
+            Debug.WriteLine("Popping {0} from the Staque: {1}ms", numberOfElements, milliseconds);
+        }
+        
+    }
+}
---
+++ Sjerrul.Utilities/Collections/Staque.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sjerrul.Utilities.Collections
+{
+    public class Staque
+    {
+        private const int _maxSize = 50;
+
+        private Node _top;
+
+        private int _count = 0;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public Staque()
+        {
+            
+        }
+        
+
+        public void Push(object data)
+        {
+            Node node = new Node(data, _top);
+            _top = node;
+
+            _count++;
+        }
+
+        public object Pop()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node nodeToreturn = _top;
+            _top = _top.GetPrevious();
+
+            _count--;
+
+            return nodeToreturn.GetData();
+        }
+
+        public object Peek()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node node = _top;
+
+            return node.GetData();
+        }
+
+
+
+        private class Node
+        {
+            private object _data;
+            private Node _previous;
+
+            public Node(object data, Node previous)
+            {
+                _data = data;
+                _previous = previous;
+            }
+
+            public object GetData()
+            {
+                return _data;
+            }
+
+            public Node GetPrevious()
+            {
+                return _previous;
+            }
+        }
+    }
+}
